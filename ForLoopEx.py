005eb1d7c5b344fb0f20f7d4fc1957cee0ad8245
Digvijay Jatkar <jatkardigvijay@gmail.com>
2022-10-25 18:21:11 +0100

    Python notes 2 keywords forloop

---
+++ ForLoopEx.py
@@ -0,0 +1,8 @@
+# Program to show the use of keywords for, while, break, continue
+for i in range(15):
+
+    print( i + 4, end = " ")
+
+    # breaking the loop when i = 9
+    if i == 9:
+        break
---
+++ KeywordsTrueOrFalse.py
@@ -0,0 +1,12 @@
+print( 4 == 4 )
+print( 6 > 9 )
+print( True or False )
+print( 9 <= 28 )
+print( 6 > 9 )
+print( True and False )
+
+#in python true is 1 and false is 0
+
+print(True == 3)
+print(False == 0)
+print(True +True + True)
